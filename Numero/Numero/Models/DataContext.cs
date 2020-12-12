7922288287e123b00d42b84edda12936d13325f8
Marcelo Ortiz Peredo <a2018119133@estudiantes.upsa.edu.bo>
2020-12-12 12:54:40 -0400

    :)

---
+++ Numero/Numero/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Numero.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")//Constructor
+        {
+
+        }
+    }
+}
---
+++ Numero/Numero/Models/Numero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Numero.Models
+{
+    public class Numero
+    {
+        
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        [Range(1, 9999)]
+        public int Numeros { get; set; }
+    }
+}
